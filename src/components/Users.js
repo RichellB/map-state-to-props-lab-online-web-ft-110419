a34456511d6a9cd60902a99c6cef31999dbe0fd0
Richell Bonfield <richell.bonfield12@gmail.com>
2020-04-13 06:43:31 +0000

    Automatically backed up by Learn

--- src/components/Users.js
+++ src/components/Users.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 class Users extends Component {
 
   render() {
+    let users = this.props.users.map((user, index) => <li key={index}>{user.username}</li>);
+
     return (
       <div>
         <ul>
