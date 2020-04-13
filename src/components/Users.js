fdef568b292d1b156ccdbeabfa65a41830d8440d
Richell Bonfield <richell.bonfield12@gmail.com>
2020-04-13 06:43:19 +0000

    Automatically backed up by Learn

--- src/components/Users.js
+++ src/components/Users.js
@@ -4,8 +4,9 @@ import { connect } from 'react-redux';
 class Users extends Component {
 
   render() {
+   let users = this.props.users.map((user, index) => <li key={index}>{user.username}</li>);
+
     return (
-      <div>
         <ul>
           Users!
           {/* Write code here that displays the usernames of all users in the Redux store */}
